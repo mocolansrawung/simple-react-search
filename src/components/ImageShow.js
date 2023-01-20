0274cfd9be92b69a7b7e95081b7e83f8824b601e
Yudho Ahmad Fahreza <69775364+mocolansrawung@users.noreply.github.com>
2023-01-20 11:56:00 +0700

    Add files via upload

---
+++ src/components/ImageShow.js
@@ -0,0 +1,10 @@
+function ImageShow(props) {
+    const { image } = props;
+    return (
+        <div>
+            <img src={image.urls.small} alt={image.alt_description} />
+        </div>
+    );
+}
+
+export default ImageShow;
